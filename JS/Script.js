a70514a2ebbab832931acf630faa2dcf979fdf57
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-03-20 22:37:33 +0300

    Features;Style the menu div

--- JS/Script.js
+++ JS/Script.js
@@ -43,131 +43,133 @@ $('#pizzaSmall').mouseleave(
     })
 
 
-// MialChimp section
-
-(function($) {
-    window.fnames = new Array();
-    window.ftypes = new Array();
-    fnames[0] = 'EMAIL';
-    ftypes[0] = 'email';
-    fnames[1] = 'FNAME';
-    ftypes[1] = 'text';
-    fnames[2] = 'LNAME';
-    ftypes[2] = 'text';
-    fnames[3] = 'ADDRESS';
-    ftypes[3] = 'address';
-    fnames[4] = 'PHONE';
-    ftypes[4] = 'phone';
-    fnames[5] = 'BIRTHDAY';
-    ftypes[5] = 'birthday';
-    fnames[6] = 'MMERGE6';
-    ftypes[6] = 'birthday';
-    fnames[7] = 'MMERGE7';
-    ftypes[7] = 'text';
-}(jQuery));
-var $mcj = jQuery.noConflict(true);
-var $mcj = jQuery.noConflict(true);
-
 
 
 
 
 // Order Section
 
-// Basic Logics
-// Initializing Values
-
-let total = 0;
-let deliveryFee = 200;
-let crustPrice = new Array(250, 200, 350);
-let sizePrice = new Array(350, 500, 700);
-let toppingsPrice = new Array(140, 180, 220);
-
-
-
-// Basic logics
-
-function Pizza(size, crust, topping) {
-    this.size = size;
-    this.crust = crust;
-    this.toppings = topping;
-
-}
-
-
-// User Interface
 
 $(document).ready(function() {
 
-    $("form#myPizza").submit(function(event) {
-        event.preventDefault();
-
-        let selectedPizzaSize = $("select#pizzaSize").find(":selected").val();;
-        let selectedPizzaCrust = $("select#pizzaCrust").find(":selected").val();
-        let selectedPizzaToppings = $("select#pizzaToppings").find(":selected").val();;
-
-        let newPizza = new Pizza(selectedPizzaSize, selectedPizzaCrust, selectedPizzaToppings);
-
-        $("#cart").hide();
-        $("#checkout").show();
+    $(".table").hide();
+    $(".button2").hide();
+    $("#more-info").hide();
+    $(".btn.yes").hide();
+    $(".btn.no").hide();
+    $("#more-info h4").hide();
+    $("#more-info h6").hide();
 
 
-        let firstOrder = sizePriceCalc(selectedPizzaSize) + crustPriceCalc(selectedPizzaCrust) + toppingPriceCalc(selectedPizzaToppings);
-
-        let orderedList = '<tr><td id="size">' + newPizza.size + '</td><td id="toppings">' + newPizza.topping + '</td><td id="crust">' + newPizza.crust + '</td><td id="total">' + firstOrder + '</td></tr>'
-
-        $("#Order").append(orderedList);
 
 
-        $("#pizzaSize").val("");
-        $("#pizzaCrust").val("");
-        $("#pizzaToppings").val("");
+    $('.btn.order').click(function() {
 
-    });
+        let pizzaSize = $("#pizzaSize option:selected").val();
+        let pizzaToppings = $("#pizzaToppings option:selected").val();
+        let pizzaCrust = $("#pizzaCrust option:selected").val();
+        let total = parseInt(pizzaSize) + parseInt(pizzaToppings) + parseInt(pizzaCrust);
 
-    var totalOrderedNo = parseInt($("#OrderedNo").val());
-
-    function calcTotalPrice() {
-        var firstPizzaPrice = sizePriceCalc(selectedPizzaSize) + crustPriceCalc(selectedPizzaCrust) + toppingPriceCalc(selectedPizzaToppings);
-
-        return firstPizzaPrice;
-    }
-    var orderedList = [];
+        var grandTotal = 0;
 
 
-    $("#orderBtn").click(function() {
-        totalOrderedNo += 1;
-        $("#orderedNo").text(totalOrderedNo);
-        orderedList.push(calcTotalPrice());
-    });
+        $("#cart").show();
+        $(".button2").show();
+        $(".btn.order").hide();
+        $(".card-text").hide();
 
-    $("#totalNo").click(function() {
-        let total = 0;
-        orderedList.forEach(function(pizza) {
-            total += pizza;
+        $("#size").html($("#pizzaSize option:selected").text() + " - " + pizzaSize);
+        $("#toppings").html($("#pizzaToppings option:selected").text() + " - " + pizzaCrust);
+        $("#crust").html($("#pizzaCrust option:selected").text() + " - " + pizzaToppings);
+        $("#total").html(total);
+
+
+
+        function Pizza(size, toppings, crust, total) {
+            this.size = size;
+            this.toppings = toppings;
+            this.crust = crust;
+            this.total = total;
+
+        }
+        $('.btn.more-pizza').click(function() {
+
+            let pizzaSize = $("#pizzaSize option:selected").val();
+            let pizzaToppings = $("#pizzaToppings option:selected").val();
+            let pizzaCrust = $("#pizzaCrust option:selected").val();
+            var total = parseInt(pizzaSize) + parseInt(pizzaToppings) + parseInt(pizzaCrust);
+
+            grandTotal = grandTotal + total;
+
+            let newPizza = new Pizza(pizzaSize, pizzaToppings, pizzaCrust, total);
+
+            let newRow = '<tr><td id="size">' + $("#pizzaSize option:selected").text() + " - " + newPizza.size + '</td><td id = "toppings">' + $("#pizzaToppings option:selected").text() + " - " + newPizza.toppings + '</td><td id="crust">' + $("#pizzaCrust option:selected").text() + " - " + newPizza.crust + '</td><td id = "total">' + newPizza.total + '<td><tr>'
+
+            $("#Order").append(newRow);
         });
-        $("#Order").text(total);
 
-        let delivery = confirm("Would you like us deliver your pizza to your doorstep? transport cost ksh 200.");
+        $(".btn.check-out").click(function() {
+            $(".btn.more-pizza").hide();
+            $(".btn.check-out").hide();
+            $("#more-info").show();
+            $(".btn.yes").show();
+            $(".btn.no").show();
+            $("#more-info .location").hide();
 
-        if (delivery) {
-            let place = prompt("Enter your location");
-            alert(`SUCCESS!!Your ordered will be delivered $ {place} and the cost is 200`)
+            grandTotal = grandTotal + total;
 
-        }
-        if (delivery) {
-            let location = prompt("Enter your location");
-            alert($("#location").text(location), $("#success").show());
-        } else {
-            $("#fail").show();
-        }
-        $("#pizzaSize").val("");
-        $("#pizzaCrust").val("");
-        $("#pizzaToppings").val("");
-        $("#Order").remove();
-        $("#OrderedNo").text(0);
+            $("#more-info h3 span").html(grandTotal);
+        });
+
+        $(".btn.yes").click(function() {
+            $("#more-info h5").hide();
+            $(".btn.yes").hide();
+            $(".btn.no").hide();
+            $("#more-info .location").show();
+            $("#more-info h3 span").html(grandTotal + 300);
+        });
+
+        $(".btn.no").click(function() {
+            $("#more-info h6").show();
+            $("#more-info h5").hide();
+            $(".btn.yes").hide();
+            $(".btn.no").hide();
+            $("#more-info .location").hide();
+        });
 
 
+        $(".btn.complete").click(function() {
+            let location = $("#more-info .location input").val();
+            $("#more-info h4").show();
+            $("#more-info .location").hide();
+            $("#more-info h4 span").html(location);
+        });
 
     });
 });
+
+
+// MialChimp section
+
+// (function($) {
+//     window.fnames = new Array();
+//     window.ftypes = new Array();
+//     fnames[0] = 'EMAIL';
+//     ftypes[0] = 'email';
+//     fnames[1] = 'FNAME';
+//     ftypes[1] = 'text';
+//     fnames[2] = 'LNAME';
+//     ftypes[2] = 'text';
+//     fnames[3] = 'ADDRESS';
+//     ftypes[3] = 'address';
+//     fnames[4] = 'PHONE';
+//     ftypes[4] = 'phone';
+//     fnames[5] = 'BIRTHDAY';
+//     ftypes[5] = 'birthday';
+//     fnames[6] = 'MMERGE6';
+//     ftypes[6] = 'birthday';
+//     fnames[7] = 'MMERGE7';
+//     ftypes[7] = 'text';
+// }(jQuery));
+// var $mcj = jQuery.noConflict(true);
+// var $mcj = jQuery.noConflict(true);
