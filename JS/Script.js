7c1092002e756abd42ee237e1865afcdbb2d88f3
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-03-21 06:35:02 +0300

    Fix;Fixing bugs

--- JS/Script.js
+++ JS/Script.js
@@ -43,12 +43,7 @@ $('#pizzaSmall').mouseleave(
     })
 
 
-
-
-
-
 // Order Section
-
 
 $(document).ready(function() {
 
@@ -153,30 +148,32 @@ $(document).ready(function() {
         });
 
     });
+
+
+
+
+    // MialChimp section
+
+    (function($) {
+        window.fnames = new Array();
+        window.ftypes = new Array();
+        fnames[0] = 'EMAIL';
+        ftypes[0] = 'email';
+        fnames[1] = 'FNAME';
+        ftypes[1] = 'text';
+        fnames[2] = 'LNAME';
+        ftypes[2] = 'text';
+        fnames[3] = 'ADDRESS';
+        ftypes[3] = 'address';
+        fnames[4] = 'PHONE';
+        ftypes[4] = 'phone';
+        fnames[5] = 'BIRTHDAY';
+        ftypes[5] = 'birthday';
+        fnames[6] = 'MMERGE6';
+        ftypes[6] = 'birthday';
+        fnames[7] = 'MMERGE7';
+        ftypes[7] = 'text';
+    }(jQuery));
+    var $mcj = jQuery.noConflict(true);
+
 });
-
-
-// MialChimp section
-
-// (function($) {
-//     window.fnames = new Array();
-//     window.ftypes = new Array();
-//     fnames[0] = 'EMAIL';
-//     ftypes[0] = 'email';
-//     fnames[1] = 'FNAME';
-//     ftypes[1] = 'text';
-//     fnames[2] = 'LNAME';
-//     ftypes[2] = 'text';
-//     fnames[3] = 'ADDRESS';
-//     ftypes[3] = 'address';
-//     fnames[4] = 'PHONE';
-//     ftypes[4] = 'phone';
-//     fnames[5] = 'BIRTHDAY';
-//     ftypes[5] = 'birthday';
-//     fnames[6] = 'MMERGE6';
-//     ftypes[6] = 'birthday';
-//     fnames[7] = 'MMERGE7';
-//     ftypes[7] = 'text';
-// }(jQuery));
-// var $mcj = jQuery.noConflict(true);
-// var $mcj = jQuery.noConflict(true);
